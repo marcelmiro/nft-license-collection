93891754c8c9179c565318ffb509078225be6e85
Marcel <marcelmiro1@hotmail.com>
2022-06-18 23:48:16 +0200

    Add footer to app home page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -6,7 +7,15 @@ const Home: NextPage = () => {
 		<div className={styles.container}>
 			<h1 className={styles.title}>NFT Licenses</h1>
 
-            <button className={styles.button}>Log in</button>
+			<button className={styles.button}>Log in</button>
+
+			<p className={styles.footer}>
+				Created by{' '}
+				<Link href="https://marcelmiro.com/">
+					<a target="_blank">Marcel</a>
+				</Link>
+				.
+			</p>
 		</div>
 	)
 }
